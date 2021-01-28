fe8526ce1985b62aab6797d63633e341880cb703
lucasljoliveira <lucasljoliveira@hotmail.com>
2021-01-28 16:12:56 -0300

    Allow empty values for category description and added tests for instance and correct values on name and description

--- back/tests/test_model_category.py
+++ back/tests/test_model_category.py
@@ -4,44 +4,60 @@ sys.path.append('.')
 from back.models.category import Category
 
 class TestCategory():
+    def test_category_type(self):
+        category = Category('nome', 'descrição')
+        assert isinstance(category, Category)
+        
+    def test_category_name(self):
+        name = 'name'
+        description = 'description'
+        category = Category(name, description)
+        assert category.name, name
+
     def test_category_name_none(self):
         try:
-            category = Category(None, 'Descrição')
+            category = Category(None, 'descrição')
             raise NotImplementedError("Error not raised")
         except Exception as error:
             assert isinstance(error, TypeError)
     
     def test_category_name_int(self):
         try:
-            category = Category(123, 'Descrição')
+            category = Category(123, 'descrição')
             raise NotImplementedError("Error not raised")
         except Exception as error:
             assert isinstance(error, TypeError)
     
     def test_category_name_emptystring(self):
         try:
-            category = Category('', 'Descrição')
+            category = Category('', 'descrição')
             raise NotImplementedError("Error not raised")
         except Exception as error:
             assert isinstance(error, ValueError)
         
     def test_category_name_space(self):
         try:
-            category = Category(' ', 'Descrição')
+            category = Category(' ', 'descrição')
             raise NotImplementedError("Error not raised")
         except Exception as error:
             assert isinstance(error, ValueError)
         
     def test_category_name_characterexcess(self):
         try:
-            category = Category('nome'*100, 'Descrição')
+            category = Category('nome'*100, 'descrição')
             raise NotImplementedError("Error not raised")
         except Exception as error:
             assert isinstance(error, ValueError)
 
+    def test_category_description(self):
+        name = 'name'
+        description = 'description'
+        category = Category(name, description)
+        assert category.description, description
+
     def test_category_description_none(self):
         try:
-            category = Category(None, 'Descrição')
+            category = Category(None, 'descrição')
             raise NotImplementedError("Error not raised")
         except Exception as error:
             assert isinstance(error, TypeError)
@@ -53,13 +69,6 @@ class TestCategory():
         except Exception as error:
             assert isinstance(error, TypeError)
     
-    def test_category_description_emptystring(self):
-        try:
-            category = Category('nome', '')
-            raise NotImplementedError("Error not raised")
-        except Exception as error:
-            assert isinstance(error, ValueError)
-        
     def test_category_description_space(self):
         try:
             category = Category('nome', ' ')
@@ -69,7 +78,7 @@ class TestCategory():
         
     def test_category_description_charactersexcess(self):
         try:
-            category = Category('nome', 'Descrição'*100)
+            category = Category('nome', 'descrição'*100)
             raise NotImplementedError("Error not raised")
         except Exception as error:
             assert isinstance(error, ValueError)
